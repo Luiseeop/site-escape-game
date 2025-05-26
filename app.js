13f46384cef7befe917ec18d079f455e62c56363
Luiseeop <luiseeop1@gmail.com>
2025-05-26 11:07:18 +0200

    ajout modif

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ fetch('escape-game.json')
   .then(data => {
 
     data.temoignages.forEach(temoi => {
-        document.getElementById("avis").innerHTML += `
-        
+    const etoilesPleines = '★'.repeat(temoi.note);
+    const etoilesVides = '☆'.repeat(5 - temoi.note);
+    const etoiles = etoilesPleines + etoilesVides;
+
+    document.getElementById("avis").innerHTML += `
         <div class="cartesa2">
-                <div class="flex spacebet div2">
-                    <p class="black">${temoi.note}/5</p>
-                    <p class="black bold">${temoi.typeExperience}</p>
-                </div>
-                <p class="black div2">${temoi.commentaire}</p>
 
-                <p class="black div2">${temoi.prenom}</p>
 
+            <div class="flex spacebet div2">
+                <p class="black size1">${etoiles}</p>
+                <p class="black bold">${temoi.typeExperience}</p>
             </div>
-        
-        
-        
-        
-        
-        `
+            <p class="black div2">${temoi.commentaire}</p>
+            <p class="black div2">${temoi.prenom}</p>
+
+            
+        </div>
 
 
+
+
+    `
 
 
 
@@ -43,11 +45,14 @@ fetch('escape-game.json')
                     <img src="${salle.image}" alt="${salle.nom}" class="imgcard">
 
                 </div>
-                <div>
+                <div class="flex">
                     <p class="black div2 bold">${salle.nom}</p>
                     <p class="black div2">${salle.description}</p>
                 </div>
-
+                <div class="carte-footer">
+                    <a href="#salles" class="whitee btnrev">Réservez</a>
+                </div>
+                 
 
             </div>
       `;
